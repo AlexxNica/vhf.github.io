35ec45646c6cfddb80aa25cd9c7e8ed13efabd3e
Victor Felder <victorfelder@gmail.com>
2016-01-16 22:59:10 +0100

    WIP: redesign

--- blog/js/script.js
+++ blog/js/script.js
@@ -1,36 +1,4 @@
 (function($) {
-  // Search
-  var $searchWrap = $('#search-form-wrap'),
-    isSearchAnim = false,
-    searchAnimDuration = 200;
-
-  var startSearchAnim = function() {
-    isSearchAnim = true;
-  };
-
-  var stopSearchAnim = function(callback) {
-    setTimeout(function() {
-      isSearchAnim = false;
-      callback && callback();
-    }, searchAnimDuration);
-  };
-
-  $('#nav-search-btn').on('click', function() {
-    if (isSearchAnim) return;
-
-    startSearchAnim();
-    $searchWrap.addClass('on');
-    stopSearchAnim(function() {
-      $('.search-form-input').focus();
-    });
-  });
-
-  $('.search-form-input').on('blur', function() {
-    startSearchAnim();
-    $searchWrap.removeClass('on');
-    stopSearchAnim();
-  });
-
   // Share
   $('body').on('click', function() {
     $('.article-share-box.on').removeClass('on');
@@ -85,53 +53,14 @@
     window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
   });
 
-  // Caption
-  // $('.article-entry').each(function(i){
-  //   $(this).find('img').each(function(){
-  //     if ($(this).parent().hasClass('fancybox')) return;
+  var $sidenav = $('.side-nav');
 
-  //     var alt = this.alt;
-
-  //     if (alt) $(this).after('<span class="caption">' + alt + '</span>');
-
-  //     $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
-  //   });
-
-  //   $(this).find('.fancybox').each(function(){
-  //     $(this).attr('rel', 'article' + i);
-  //   });
-  // });
-
-  // if ($.fancybox){
-  //   $('.fancybox').fancybox();
-  // }
-
-  // Mobile nav
-  var $container = $('#container'),
-    isMobileNavAnim = false,
-    mobileNavAnimDuration = 200;
-
-  var startMobileNavAnim = function() {
-    isMobileNavAnim = true;
-  };
-
-  var stopMobileNavAnim = function() {
-    setTimeout(function() {
-      isMobileNavAnim = false;
-    }, mobileNavAnimDuration);
-  }
-
-  $('#main-nav-toggle').on('click', function() {
-    if (isMobileNavAnim) return;
-
-    startMobileNavAnim();
-    $container.toggleClass('mobile-nav-on');
-    stopMobileNavAnim();
+  $('#mobile-menu').click(function(e) {
+    e.stopPropagation();
+    $sidenav.css('left', 0);
+  });
+  $('#mobile-menu-close').click(function() {
+    $sidenav.css('left', '-105%');
   });
 
-  $('#wrap').on('click', function() {
-    if (isMobileNavAnim || !$container.hasClass('mobile-nav-on')) return;
-
-    $container.removeClass('mobile-nav-on');
-  });
 })(jQuery);
