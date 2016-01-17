9977bd94d5bbc878ccc4eb526f0acce4b5be365b
Victor Felder <victorfelder@gmail.com>
2016-01-17 01:11:09 +0100

    Minify and clean

--- blog/js/script.js
+++ blog/js/script.js
@@ -1,8 +1,8 @@
-(function($) {
+(function($){
   // Share
-  $('body').on('click', function() {
+  $('body').on('click', function(){
     $('.article-share-box.on').removeClass('on');
-  }).on('click', '.article-share-link', function(e) {
+  }).on('click', '.article-share-link', function(e){
     e.stopPropagation();
 
     var $this = $(this),
@@ -11,23 +11,23 @@
       id = 'article-share-box-' + $this.attr('data-id'),
       offset = $this.offset();
 
-    if ($('#' + id).length) {
+    if ($('#' + id).length){
       var box = $('#' + id);
 
-      if (box.hasClass('on')) {
+      if (box.hasClass('on')){
         box.removeClass('on');
         return;
       }
     } else {
       var html = [
         '<div id="' + id + '" class="article-share-box">',
-        '<input class="article-share-input" value="' + url + '">',
-        '<div class="article-share-links">',
-        '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
-        '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-        '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
-        '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
-        '</div>',
+          '<input class="article-share-input" value="' + url + '">',
+          '<div class="article-share-links">',
+            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
+            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
+            '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
+            '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
+          '</div>',
         '</div>'
       ].join('');
 
@@ -42,11 +42,11 @@
       top: offset.top + 25,
       left: offset.left
     }).addClass('on');
-  }).on('click', '.article-share-box', function(e) {
+  }).on('click', '.article-share-box', function(e){
     e.stopPropagation();
-  }).on('click', '.article-share-box-input', function() {
+  }).on('click', '.article-share-box-input', function(){
     $(this).select();
-  }).on('click', '.article-share-box-link', function(e) {
+  }).on('click', '.article-share-box-link', function(e){
     e.preventDefault();
     e.stopPropagation();
 
@@ -55,11 +55,11 @@
 
   var $sidenav = $('.side-nav');
 
-  $('#mobile-menu').click(function(e) {
+  $('#mobile-menu').click(function (e) {
     e.stopPropagation();
     $sidenav.css('left', 0);
   });
-  $('#mobile-menu-close').click(function() {
+  $('#mobile-menu-close').click(function () {
     $sidenav.css('left', '-105%');
   });
 
